51a9de49dc5a3ee85a44770b3e7d9505f95804f4
Churchil Owino <chachowino75@gmail.com>
2024-10-17 19:50:35 +0300

    Add styling and layout basic components

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,14 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
@@ -24,7 +36,9 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     GreetingText(
                         name = stringResource(id = R.string.greeting_string),
-                        modifier = Modifier.padding(innerPadding)
+                        from = stringResource(id = R.string.from_string),
+                        modifier = Modifier
+                            .padding(innerPadding)
                     )
                 }
             }
@@ -33,19 +47,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingText(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = name,
-        fontSize = 100.sp,
-        lineHeight = 116.sp,
-        modifier = modifier
-    )
+fun GreetingText(
+    name: String,
+    from: String,
+    modifier: Modifier = Modifier
+) {
+    val gradientColors = listOf(Color.Cyan, Color.Blue, Color.Magenta /*...*/)
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        Text(
+            text = name,
+            style = TextStyle(
+                shadow = Shadow(
+                    color = Color.Blue,
+                    offset = Offset(5.0f, 10.0f),
+                    blurRadius = 3f
+                ),
+                fontSize = 100.sp,
+                fontStyle = FontStyle.Italic,
+                fontWeight = FontWeight.Bold,
+                lineHeight = 120.sp,
+                textAlign = TextAlign.Center,
+                brush = Brush.linearGradient(gradientColors)
+            ),
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp,
+            color = Color.DarkGray,
+            modifier = Modifier
+                .padding(16.dp)
+                .align(alignment = Alignment.End)
+        )
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true, name = "My Preview")
 @Composable
 fun GreetingPreview() {
     HappyBirthdayTheme {
-        GreetingText("Happy Birthday Sheila")
+        GreetingText("Happy Birthday World!", "From Churchil")
     }
 }
